5ebc917f9cf8980826469a33c58219966602772e
FirsT2015 <you@example.com>
2023-12-21 20:25:40 +0100

    uiuihb

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,22 +1,14 @@
-'use client'
+
 import React from 'react'
 import './globals.css'
-import { CirclesWithBar } from 'react-loader-spinner'
+import AppLoader from '@/components/loading'
+
 function Loading() {
   return (
     <div className=' bg-black justify-center flex align-middle items-center m-9 w-full h-[100vh]' >  
-        <CirclesWithBar
-        height="100"
-        width="100"
-        color="#4fa94d"
-        outerCircleColor="#4fa94d"
-        innerCircleColor="#4fa94d"
-        barColor="#4fa94d"
-        ariaLabel="circles-with-bar-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        /></div>
+    <AppLoader />
+ </div>
+
   )
 }
 
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 export default function NotFound () {
     return (
         <div>
-
 <div className="w-full h-screen flex flex-col md:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
     <div className="w-full md:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
         <p className="text-4xl md:text-[4rem] lg:text-[5rem] font-bold tracking-wider text-gray-300">404</p>
@@ -20,7 +19,6 @@ export default function NotFound () {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" className="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
 </svg>
-
             <span>Contact Us</span>
         </Link>
     </div>
