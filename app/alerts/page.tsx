3202139504a8a50aac0225a4895f30f2d15bc0c2
FirsT2015 <you@example.com>
2023-12-28 08:49:30 +0100

    hi1234

---
+++ app/alerts/page.tsx
@@ -0,0 +1,14 @@
+import Alerts from '@/components/alert component/alerts'
+import Footer2 from '@/components/features component/footer2'
+import React from 'react'
+
+function AlertPage() {
+  return (
+    <div className=' w-[100vw] overflow-hidden'>
+        <Alerts />
+        <Footer2 />
+    </div>
+  )
+}
+
+export default AlertPage
---
+++ components/alert component/alerts.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Alerts() {
+  return (
+    <div>
+        <div className='flex flex-col md:hidden'>
+            <h1 className="flex justify-center align-middle items-start m-10 text-slate-300 text-2xl font-heading ">
+             View All Alerts and Notifications
+            </h1>
+            <div>
+            <h1 className="flex justify-center align-middle items-start m-5 text-slate-300 text-heading font-heading ">
+             View your transactions alerts
+            </h1>
+              <div className="w-full mb-2 select-none border-l-4 border-green-400 bg-green-100 p-4 font-medium hover:border-green-500">    No Transaction Alert yet!</div>   
+            </div>
+            <div>
+            <h1 className="flex justify-center align-middle items-start m-5 text-slate-300 text-heading font-heading ">
+             View your bets alerts
+            </h1>
+            <div className="w-full mb-2 select-none rounded-l-lg border-l-4 border-yellow-400 bg-yellow-100 p-4 font-medium hover:border-yellow-500">No Bet Alerts Yet!</div>
+            </div>
+            <div>
+            <h1 className="flex justify-center align-middle items-start m-5 text-slate-300 text-heading font-heading ">
+             Warning from Beta Sporting
+            </h1>
+            <div className="w-full mb-2 select-none rounded-t-lg border-t-4 border-yellow-400 bg-yellow-100 p-4 font-medium hover:border-yellow-500">No Warning yet!</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Alerts
