9e998de2f21e040a3c8834db00b2e0c5a5019ea7
FirsT2015 <you@example.com>
2023-12-30 10:51:45 +0100

    helo

--- app/football/serie-a/page.tsx
+++ app/football/serie-a/page.tsx
@@ -1,17 +1,24 @@
-import { filterLeague } from '../../api'
-import LeagueTable from "../../../components/football component/LeagueTable"
+import { filterLeague } from '../../api';
+import LeagueTable from "../../../components/football component/LeagueTable";
+import SmSidebar from '@/components/football component/smsidebar';
 
-const SerieA= async () => {
-  const getSerieA= await filterLeague('Serie A')
-  return (
-    <div className='w-[100vw] px-4 md:px-0 md:w-[650px]'>
-      {getSerieA.map((data) => (
+const SerieA = async () => {
+ const getSerieA = await filterLeague('Serie A');
+
+ return (
+  <div className='w-[100vw] px-4 md:px-0 md:w-[650px]'>
+    <SmSidebar />
+    {getSerieA.length > 0 ? (
+      getSerieA.map((data) => (
         <div key={data.id}>
           <LeagueTable data={data} />
         </div>
-      ))}
-    </div>
-  )
-}
+      ))
+    ) : (
+<p className='  text-xl flex justify-center items-center align-middle md:text-2xl font-bold m-5 text-slate-300'>No fixtures</p>
+     )}
+  </div>
+ );
+};
 
-export default SerieA
+export default SerieA;
