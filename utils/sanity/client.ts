cca960d475ae27cdd5278f12da374e5d3269ee21
FirsT2015 <you@example.com>
2023-12-30 02:46:42 +0100

    rdrd64

--- utils/sanity/client.ts
+++ utils/sanity/client.ts
@@ -1,4 +1,5 @@
-import {createClient} from 'next-sanity'
+import {createClient} from 'next-sanity';
+
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID 
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
 
